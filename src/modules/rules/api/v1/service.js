fe6e74e249db34d79d30b5f6b15f7deff10dd48d
Akalanka <akalankaperera128@gmail.com>
2023-06-13 23:29:34 +0530

    Refactored engine code

--- src/modules/rules/api/v1/service.js
+++ src/modules/rules/api/v1/service.js
@@ -1,8 +1,8 @@
 import { traced } from '@sliit-foss/functions';
 import { default as createError } from 'http-errors';
-import { default as RuleEngine } from '../../utils/engine';
+import { RuleEngine } from '../../../../utils';
 import { saveRule, retrieveRuleById, retrieveRuleByName, retrieveRules, updateRuleById, deleteRuleById } from '../../repository';
-import { ruleKey } from '../../utils';
+import { ruleKey } from '../../constants';
 import { redis } from '../../../../database/redis';
 
 export const addRule = (rule) => {
@@ -18,18 +18,22 @@ export const getRules = (filters, sorts, page, limit) => {
 };
 
 export const updateRule = (id, payload) => {
-  return traced(updateRuleById)(id, payload);
+  return traced(updateRuleById)(id, payload).then((rule) => {
+    redis.set(ruleKey(rule), rule);
+    return rule;
+  });
 };
 
 export const deleteRule = (id) => {
-  return traced(deleteRuleById)(id);
+  return traced(deleteRuleById)(id).then((rule) => {
+    redis.del(ruleKey(rule));
+    return rule;
+  });
 };
 
 export const processRule = async (rule, facts) => {
   rule = await redis.getOrDefault(ruleKey(rule), () => traced(retrieveRuleByName)(rule));
-
   if (!rule) throw createError(404, "Rule not found")
-
   const engine = new RuleEngine([rule], { allowUndefinedFacts: true });
   return engine.run(facts);
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 export * from './constants';
 export * from './response';
+export { default as RuleEngine } from './engine';
